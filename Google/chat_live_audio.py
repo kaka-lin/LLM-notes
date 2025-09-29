415b23a3c83e5a4d6712fa8ef2cf4bb2ccf7a44c
Kaka <vn503024@gmail.com>
2025-09-30 02:37:40 +0800

    feat(gemini): added the example of Gemini Live API

---
+++ Google/chat_live_audio.py
@@ -0,0 +1,18 @@
+import asyncio
+from dotenv import load_dotenv
+
+from gemini_agent import GeminiAgent
+
+
+async def main():
+    # 載入 .env 內容到環境變數，並強制更新
+    if not load_dotenv(override=True):
+        print("警告：.env 檔案不存在或解析失敗，請確認它位於專案根目錄。")
+
+    input_text = "Hello? Gemini are you there?"
+    gemini_agent = GeminiAgent(is_live=True, is_audio=True)
+    await gemini_agent.process(input_text)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- Google/gemini_agent.py
+++ Google/gemini_agent.py
@@ -3,6 +3,8 @@ import os
 from google import genai
 from google.genai import types
 
+from wav_utils import wave_file
+
 
 class GeminiAgent:
     """
@@ -10,15 +12,25 @@ class GeminiAgent:
     including ASR, translation, dialogue, Q&A, etc.
     """
     def __init__(
-        self, model_name: str = "gemini-2.5-flash",
-        gemini_api_key: str = ""
+        self,
+        model_name: str = "gemini-2.5-flash",
+        live_model_name: str = "gemini-live-2.5-flash-preview",
+        gemini_api_key: str = "",
+        is_live: bool = False,
+        is_audio: bool = False,
+        temperature: float = 0.0,
+        max_output_tokens: int = 100,
+        timeout: int = 5,
     ):
         self.model_name = model_name
+        self.live_model_name = live_model_name
+        self.is_live = is_live
+        self.is_audio = is_audio
 
         # Get agent configurations
-        self.temperature = 0.0
-        self.max_output_tokens = 100
-        self.timeout = 5
+        self.temperature = temperature
+        self.max_output_tokens = max_output_tokens
+        self.timeout = timeout
 
         # Get Gemini API key from config or environment variable
         self.api_key = gemini_api_key or os.getenv("GEMINI_API_KEY")
@@ -33,6 +45,15 @@ class GeminiAgent:
 
     def process(self, input_text: str) -> str:
         """使用 Google Gemini API 處理輸入"""
+        if self.is_audio:
+            return self._live_process_audio(input_text)
+        elif self.is_live:
+            return self._live_process(input_text)
+        else:
+            return self._process(input_text)
+
+    def _process(self, input_text: str) -> str:
+        """使用 Google Gemini 一般模型 API 處理輸入"""
         # ==================================================================
         #                        FOR ERROR TESTING
         # ==================================================================
@@ -77,3 +98,62 @@ class GeminiAgent:
         # The response structure has also changed.
         # It's better to access the text via response.text
         return response.text.strip()
+
+    async def _live_process(self, input_text: str):
+        """使用 Google Gemini Live 模型 API 處理輸入"""
+        config = {
+            "response_modalities": ["TEXT"]
+        }
+
+        async with self.client.aio.live.connect(
+            model=self.live_model_name, config=config
+        ) as session:
+            print(f"> {input_text}\n")
+            await session.send_client_content(
+                turns={"role": "user", "parts": [{"text": input_text}]},
+                turn_complete=True
+            )
+
+            # For text responses, When the model's turn is complete it
+            # breaks out of the loop.
+            turn = session.receive()
+            async for chunk in turn:
+                if chunk.text is not None:
+                    print(f'- {chunk.text}')
+
+    @staticmethod
+    async def async_enumerate(aiterable):
+        """為非同步迭代器加上索引"""
+        n = 0
+        async for item in aiterable:
+            yield n, item
+            n += 1
+
+    async def _live_process_audio(self, input_text: str):
+        """使用 Google Gemini Live 模型 API 處理輸入"""
+        config = {
+            "response_modalities": ["AUDIO"]
+        }
+
+        async with self.client.aio.live.connect(
+            model=self.live_model_name, config=config
+        ) as session:
+            file_name = 'audio.wav'
+            with wave_file(file_name) as wav:
+                print(f"> {input_text}\n")
+                await session.send_client_content(
+                    turns={"role": "user", "parts": [{"text": input_text}]},
+                    turn_complete=True
+                )
+
+                # For text responses, When the model's turn is complete it
+                # breaks out of the loop.
+                turn = session.receive()
+                async for n, response in self.async_enumerate(turn):
+                    if response.data is not None:
+                        wav.writeframes(response.data)
+
+                        if n == 0:
+                            inline_data = response.server_content.model_turn.parts[0].inline_data
+                            print(inline_data.mime_type)
+                        print('.', end='')
